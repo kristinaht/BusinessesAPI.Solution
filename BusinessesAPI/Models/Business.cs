3071a86568672d2dff29d588b53bdc6624de7e84
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-03 09:21:45 -0700

    create Business class

---
+++ BusinessesAPI/Models/Business.cs
@@ -0,0 +1,9 @@
+namespace BusinessesApi
+{
+  public class Business
+  {
+    public int BusinessId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+  }
+}
