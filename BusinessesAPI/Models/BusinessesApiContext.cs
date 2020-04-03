c4c0796428635a3a784ea906c53f2dbe3cb8be48
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-03 11:23:07 -0700

    seed the database

--- BusinessesAPI/Models/BusinessesApiContext.cs
+++ BusinessesAPI/Models/BusinessesApiContext.cs
@@ -10,5 +10,16 @@ namespace BusinessesApi.Models
     }
 
     public DbSet<Business> Businesses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Business>()
+        .HasData(
+          new Business { BusinessId=1, Name="Piazza Italia", Description="Italian Restaurant"},
+          new Business { BusinessId=2, Name="Nordstrom", Description="Department store"},
+          new Business { BusinessId=3, Name="New Seasons", Description="Grocery store"},
+          new Business { BusinessId=4, Name="Umbria", Description="Caffee"}
+        );
+    }
   }
 }
